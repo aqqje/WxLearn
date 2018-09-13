208c733452a41470a1abb396451ed4657105b14f
aqqje <1042136232@qq.com>
2018-09-13 22:38:00 +0800

    updata

--- Mini program/pages/list/list.js
+++ Mini program/pages/list/list.js
@@ -1,18 +1,22 @@
 // pages/list/list.js
+let datas=require("../../datas/list-data.js")
+console.log(datas , typeof datas)
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    dataArray: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      dataArray: datas.list_data
+    });
   },
 
   /**
