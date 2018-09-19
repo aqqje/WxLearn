1fff7793a21a1dad8b8d0750bfddcadc315cf6ee
aqqje <1042136232@qq.com>
2018-09-19 21:33:36 +0800

    set storage

--- Mini program/pages/detail/detail.js
+++ Mini program/pages/detail/detail.js
@@ -8,6 +8,7 @@ Page({
   data: {
     dataObj: [],
     index: null,
+    isCollection: false
   },
 
   /**
@@ -20,13 +21,58 @@ Page({
       dataObj: datas.list_data[index],
       index
     });
-  },
+    
+    let isDetailCollected =  wx.getStorageSync("isCollected");
+    // 对缓存对象进行 init
+    if (!isDetailCollected){
+      wx.setStorageSync("isCollected", {})
+    }
+    // 判断当前用户是收藏当前文章
+    if (isDetailCollected[index]){
+      this.setData({
+        isCollection: isDetailCollected
+      })
+    }
 
+  },
+  // 是否显示收藏
+  handleCollection() {
+    let isCollection = !this.data.isCollection;
+    this.setData({
+      isCollection: isCollection
+    })
+    let title = isCollection ? "收藏成功" :"取消收藏"
+    wx.showToast({
+      title: title,
+      icon: "success"
+    });
+    // 获取本地缓存数据
+    wx.getStorage({
+      key: "isCollected",
+      success: (datas) => {
+        // console.log(datas, typeof datas);
+        // 设置本地缓存数据
+        let { index } = this.data;
+        let obj = datas.data;
+        obj[index] = isCollection;
+        wx.setStorage({
+          key: "isCollected",
+          data: obj,
+          success: () => {
+            console.log("缓存成功！")
+          }
+        });
+      }
+    })
+
+
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log(this)
   },
 
   /**
